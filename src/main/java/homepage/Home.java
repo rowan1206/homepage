9e26ac01e469b9d1d81bc2cae06ddbb963a7d91f
rowan <rowan@yahoo.com>
2019-11-25 22:15:41 +0900

    update home

--- src/main/java/homepage/Home.java
+++ src/main/java/homepage/Home.java
@@ -2,6 +2,7 @@ package homepage;
 
 public class Home {
 	public static void main(String[] args) {
-		System.out.println("nba");
+		System.out.println("nba hahah");
+		System.out.println("cba");
 	}
 }
