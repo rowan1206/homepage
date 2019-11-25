936a2cb047217f40ccaaa054bb93fecdbac90183
rowan <rowan@yahoo.com>
2019-11-25 22:48:17 +0900

    babab

--- src/main/java/homepage/Home.java
+++ src/main/java/homepage/Home.java
@@ -2,7 +2,7 @@ package homepage;
 
 public class Home {
 	public static void main(String[] args) {
-		System.out.println("nba hahah bbbbbb");
+		System.out.println("nba hahah bbbbbb bbbbb");
 		System.out.println("cba");
 	}
 }
